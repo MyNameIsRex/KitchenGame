5f7eaf23743439c30e4510c1ef610693c7d6fa5d
MyNameIsRex <rex52836@gmail.com>
2023-11-23 21:02:25 +0800

    Update v0.1.0.5_7: Renamed MultiTextureObject to MultiStateObject

--- core/src/data/psychologytheory/kitchengame/gameplay/gameobjects/InteractableGameObject.java
+++ core/src/data/psychologytheory/kitchengame/gameplay/gameobjects/InteractableGameObject.java
@@ -2,7 +2,7 @@ package data.psychologytheory.kitchengame.gameplay.gameobjects;
 
 import com.badlogic.gdx.graphics.Texture;
 
-public class InteractableGameObject extends MultiTextureGameObject {
+public class InteractableGameObject extends MultiStateGameObject {
     public InteractableGameObject(int objID, float objWidth, float objHeight, float objPosX, float objPosY, String objName, Texture[] textures, int zIndex, boolean canMove, boolean canInteract) {
         super(objID, objWidth, objHeight, objPosX, objPosY, objName, textures, zIndex, canMove, canInteract);
     }
--- core/src/data/psychologytheory/kitchengame/gameplay/gameobjects/MultiStateGameObject.java
+++ core/src/data/psychologytheory/kitchengame/gameplay/gameobjects/MultiStateGameObject.java
@@ -2,13 +2,13 @@ package data.psychologytheory.kitchengame.gameplay.gameobjects;
 
 import com.badlogic.gdx.graphics.Texture;
 
-public class MultiTextureGameObject extends AbstractGameObject {
+public class MultiStateGameObject extends AbstractGameObject {
     private Texture currentTexture;
     private Texture[] textures;
     private boolean canMove;
     private boolean canInteract;
 
-    public MultiTextureGameObject(int objID, float objWidth, float objHeight, float objPosX, float objPosY, String objName, Texture[] textures, int zIndex, boolean canMove, boolean canInteract) {
+    public MultiStateGameObject(int objID, float objWidth, float objHeight, float objPosX, float objPosY, String objName, Texture[] textures, int zIndex, boolean canMove, boolean canInteract) {
         super(objID, objWidth, objHeight, objPosX, objPosY, objName, textures[0], zIndex);
         this.currentTexture = textures[0];
         this.textures = textures;
