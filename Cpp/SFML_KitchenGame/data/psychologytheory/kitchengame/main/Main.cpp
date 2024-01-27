772b2d846a76f77d10a879c2849eed8d85a27063
MyNameIsRex <rex52836@gmail.com>
2024-01-27 09:17:03 +0800

    [CPP] Initial Test of SFML2 Installation
    SFML2 is working

--- Cpp/SFML_KitchenGame/data/psychologytheory/kitchengame/main/Main.cpp
+++ Cpp/SFML_KitchenGame/data/psychologytheory/kitchengame/main/Main.cpp
@@ -1,7 +1,26 @@
-#include <stdio.h>
+#include <SFML/Graphics.hpp>
 
 int main()
 {
-	printf("Hello World!");
+	sf::RenderWindow window(sf::VideoMode(200, 200), "Test SFML!");
+	sf::CircleShape shape(100.0F);
+	shape.setFillColor(sf::Color::Green);
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+			{
+				window.close();
+			}
+		}
+
+		window.clear();
+		window.draw(shape);
+		window.display();
+	}
+
 	return 0;
 }
